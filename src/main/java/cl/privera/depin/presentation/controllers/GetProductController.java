69edfec799bb910f835fee67f42234bc6270364c
Patricio Rivera <privera@hasar.cl>
2024-05-11 23:33:47 -0400

    Se eliminaron las excepciones de los controladores en favor del uso de ResponseEntity<T>

--- src/main/java/cl/privera/depin/presentation/controllers/GetProductController.java
+++ src/main/java/cl/privera/depin/presentation/controllers/GetProductController.java
@@ -5,6 +5,7 @@ import cl.privera.depin.application.dtos.ProductDto;
 import cl.privera.depin.presentation.exceptions.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,13 +18,13 @@ public class GetProductController {
     private IGetProductHandler getProductHandler;
 
     @GetMapping("/products/{id}")
-    public ProductDto getSingle(@PathVariable Integer id) {
+    public ResponseEntity<ProductDto> getSingle(@PathVariable Integer id) {
         log.info("ProductController: get {}", id);
         var dto = getProductHandler.handle(id);
         if (dto == null) {
-            throw new EntityNotFoundException();
+            ResponseEntity.notFound();
         }
 
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/cl/privera/depin/presentation/exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package cl.privera.depin.presentation.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Entity not found")
-public class EntityNotFoundException extends RuntimeException {
-}
